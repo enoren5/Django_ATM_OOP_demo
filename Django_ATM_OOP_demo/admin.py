0f1c7c59e8143441a6ceebe25fcb937da5633694
Pete Naveen Pearson <warden496@protonmail.com>
2022-05-06 08:30:16 -0400

    Wire up and port basic model classes from script

---
+++ Django_ATM_OOP_demo/admin.py
@@ -0,0 +1,4 @@
+from .models import Accounts
+
+
+admin.site.register(Accounts)
--- telagents/views.py
+++ telagents/views.py
@@ -4,3 +4,28 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "telagents/home.html")
+
+'''
+    def deposit(self, amount):
+        self.balance += round(Decimal(amount),2)
+        self.transaction_id += randint(101,999) - randint(101,999) 
+        return f'D-{self.account_num}-{self.tzone.condensed()}-{self.transaction_id}'
+    
+    def withdraw(self, amount):
+        if amount > self.balance:         
+            self.transaction_id += randint(101,999) - randint(101,999) 
+            print(f'X-{self.account_num}-{self.tzone.condensed()}-{self.transaction_id}') 
+            raise ValueError('Transaction declined. Insufficient funds. Please deposit some more $$$ first.')
+            
+        self.balance -= round(Decimal(amount),2)
+        self.transaction_id += randint(101,999) - randint(101,999) 
+        return f'W-{self.account_num}-{self.tzone.condensed()}-{self.transaction_id}'
+
+    def pay_interest(self):
+        monthly_rate = self.interest/12
+        monthly_sum = monthly_rate * self.balance
+        self.transaction_id += randint(101,999) - randint(101,999) 
+        print(f'I-{self.account_num}-{self.tzone.condensed()}-{self.transaction_id}') 
+        return round(Decimal(monthly_sum, + self.balance), 2)
+'''
+
