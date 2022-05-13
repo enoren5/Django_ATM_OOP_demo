c688944e8d4e942a1fea9cf00d6af66c76dfc6b9
Pete Naveen Pearson <warden496@protonmail.com>
2022-05-13 10:09:39 -0400

    Initialize views logic and data points in template

--- telagents/views.py
+++ telagents/views.py
@@ -4,8 +4,22 @@ from telagents.models import Account
 
 def index(request):
     #context = Account(request)
+    balance = 0
+    withdrawal = 0
+    deposit = 0
+    amount = 0
     data = Account.objects.all().order_by('-inception_date')
-    context = {'data':data}
+    context = {'data':data,'withdrawal':withdrawal,'deposit':deposit,'amount':amount, 'balance': balance,} # initialize context var to be updated after math operations
+    if 'amount' in request.POST:
+        if 'deposit':   
+            balance = balance + amount
+       
+        if 'withdrawal' :
+            balance  = balance - amount
+            
+        context.update(
+            {'data':data,'withdrawal':withdrawal,'deposit':deposit,'amount':amount, 'balance': balance,}, 
+        )
     return render(request, "telagents/home.html", context)
 
 '''
