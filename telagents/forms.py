278f2857d00c20b44462512165d0c5ae5fb45fa2
Pete Naveen Pearson <warden496@protonmail.com>
2022-05-20 13:29:18 -0400

    Reformat transaction objs context loop

--- telagents/forms.py
+++ telagents/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 class AmountForm(forms.Form):
     amount = forms.DecimalField(label='Amount', max_digits=10, decimal_places=2)
-    #deposit = forms.DecimalField(label='Deposit', max_digits=10, decimal_places=2)
+    # deposit = forms.DecimalField(label='Deposit', max_digits=10, decimal_places=2)
     # withdraw = forms.DecimalField(label='Withdraw', max_digits=10, decimal_places=2)
 
