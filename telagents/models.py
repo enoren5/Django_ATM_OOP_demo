39d4dc6dcd25675fc042e0fbaebec9cde38b45a1
Pete Naveen Pearson <warden496@protonmail.com>
2022-06-10 03:26:48 -0400

    Brand New User, BankAccount, Transaction

--- telagents/models.py
+++ telagents/models.py
@@ -7,34 +7,46 @@ from random import randint
 from django.forms import ModelForm
 from telagents.forms import AmountForm
 
-class Account(models.Model):
-    #### Static data elements :
-    interest = models.DecimalField(max_digits=6, decimal_places=3) # Percent
-    inception_date = models.DateTimeField('Client since (inception date)')
+class User(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    account_number = models.BigIntegerField()
-    #### Interactive and dynamic data points :
+    client_since = models.DateTimeField('Client since')
+    access_card = models.IntegerField(max_length=16)
+    PIN = models.IntegerField(max_length=4)
+
+    def creation_date(self):
+        # a = self.pub_date.timezone.now("US")
+        # b = pytz.timezone("US")
+        return self.client_since.strftime("%A %d %B %Y @ %I:%M:%S %p")
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}'s bank account."
+    
+class BankAccount(models.Model):
+    interest = models.DecimalField(max_digits=6, decimal_places=3) # Percent
+    account_opened = models.DateTimeField('Inception date')
+    transit_number = models.IntegerField(max_length=5)
+    account_number = models.BigIntegerField(max_length=6)   
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.transit_number} / {self.account_number}"
+
+class Transaction(models.Model):
     debit = models.DecimalField(max_digits=12, decimal_places=2)
     credit = models.DecimalField(max_digits=12, decimal_places=2)
     balance = models.DecimalField(max_digits=12, decimal_places=2)
     amount = models.DecimalField(max_digits=12, decimal_places=2)
     trans_timestamp = models.DateTimeField(auto_now=False, auto_now_add=False)
     trans_id = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account = models.ForeignKey(BankAccount, on_delete=models.CASCADE)
 
-    def client_since(self):
-        # a = self.pub_date.timezone.now("US")
-        # b = pytz.timezone("US")
-        return self.inception_date.strftime("%A %d %B %Y @ %I:%M:%S %p")
-    
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}'s bank account."
-
-class Transactions(ModelForm):
+'''
     class Meta:
         model = Account
         fields = ['amount',]
- 
+ '''
 
 '''
     account_num = randint(9999999,99999999)
